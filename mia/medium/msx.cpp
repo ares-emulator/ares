963086edb54ce05ec81868400dc8b7df0202aa58
Luke Usher <luke.usher@outlook.com>
2021-07-10 00:39:11 +0100

    mia: use database for msx

--- mia/medium/msx.cpp
+++ mia/medium/msx.cpp
@@ -15,8 +15,10 @@ auto MSX::load(string location) -> bool {
   }
   if(!rom) return false;
 
+  this->sha256   = Hash::SHA256(rom).digest();
   this->location = location;
-  this->manifest = analyze(rom);
+  this->manifest = Medium::manifestDatabase(sha256);
+  if(!manifest) manifest = analyze(rom);
   auto document = BML::unserialize(manifest);
   if(!document) return false;
 
