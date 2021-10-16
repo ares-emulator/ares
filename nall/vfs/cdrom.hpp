2dbd5a43a0593b5a294ed5bae4e1e8f41fc1946a
TascoDLX <tasco.deluxe@gmail.com>
2021-10-16 00:20:02 -0400

    Improved CD CUEsheet loading to handle PREGAPs & POSTGAPs

--- nall/vfs/cdrom.hpp
+++ nall/vfs/cdrom.hpp
@@ -51,22 +51,49 @@ private:
     CD::Session session;
     session.leadIn.lba = -LeadInSectors;
     session.leadIn.end = -1;
-    s32 lbaDisc = Track1Pregap;
-    s32 endDisc = lbaDisc;
+    s32 lbaFileBase = 0;
+
+    // add 2 sec pregap to 1st track
+    if(!cuesheet.files[0].tracks[0].pregap)
+      cuesheet.files[0].tracks[0].pregap = Track1Pregap;
+    else
+      cuesheet.files[0].tracks[0].pregap = Track1Pregap + cuesheet.files[0].tracks[0].pregap();
+
+    if(cuesheet.files[0].tracks[0].indices[0].number == 1) {
+      session.tracks[1].indices[0].lba = 0;
+      session.tracks[1].indices[0].end =
+          cuesheet.files[0].tracks[0].pregap() + cuesheet.files[0].tracks[0].indices[0].lba - 1;
+    }
+
+    s32 lbaIndex = 0;
     for(auto& file : cuesheet.files) {
       for(auto& track : file.tracks) {
         session.tracks[track.number].control = track.type == "audio" ? 0b0000 : 0b0100;
-        session.tracks[track.number].address = 0b0001;
+        if(track.pregap) lbaFileBase += track.pregap();
         for(auto& index : track.indices) {
-          session.tracks[track.number].indices[index.number].lba = lbaDisc + index.lba;
-          session.tracks[track.number].indices[index.number].end = lbaDisc + index.end;
+          if(index.lba >= 0) {
+            session.tracks[track.number].indices[index.number].lba = lbaFileBase + index.lba;
+            session.tracks[track.number].indices[index.number].end = lbaFileBase + index.end;
+            if(index.number == 0 && track.pregap) {
+              session.tracks[track.number].indices[index.number].lba -= track.pregap();
+              session.tracks[track.number].indices[index.number].end -= track.pregap();
+            }
+          } else {
+            // insert gap
+            session.tracks[track.number].indices[index.number].lba = lbaIndex;
+            if(index.number == 0)
+              session.tracks[track.number].indices[index.number].end = lbaIndex + track.pregap() - 1;
+            else
+              session.tracks[track.number].indices[index.number].end = lbaIndex + track.postgap() - 1;
+          }
+          lbaIndex = session.tracks[track.number].indices[index.number].end + 1;
         }
+        if(track.postgap) lbaFileBase += track.postgap();
       }
-      lbaDisc += file.tracks.last().indices.last().end + 1;
-      endDisc = lbaDisc;
+      lbaFileBase = lbaIndex;
     }
-    session.leadOut.lba = endDisc;
-    session.leadOut.end = endDisc + LeadOutSectors - 1;
+    session.leadOut.lba = lbaFileBase;
+    session.leadOut.end = lbaFileBase + LeadOutSectors - 1;
 
     for(u32 track : range(100)) {
       if(!session.tracks[track]) continue;
@@ -90,27 +117,25 @@ private:
       break;
     }
 
-    session.tracks[1].indices[0].lba = 0;  //track 1, index 0 is not present in CUE files
-    session.tracks[1].indices[0].end = Track1Pregap - 1;
+    _image.resize(2448 * (LeadInSectors + lbaFileBase + LeadOutSectors));
 
-    _image.resize(2448 * (LeadInSectors + endDisc + LeadOutSectors));
-
-    lbaDisc = Track1Pregap;
+    lbaFileBase = 0;
     for(auto& file : cuesheet.files) {
       auto location = string{Location::path(cueLocation), file.name};
       auto filedata = nall::file::open(location, nall::file::mode::read);
       if(file.type == "wave") filedata.seek(44);  //skip RIFF header
-      u64 offset = 0;
       for(auto& track : file.tracks) {
+        if(track.pregap) lbaFileBase += track.pregap();
         for(auto& index : track.indices) {
+          if(index.lba < 0) continue; // ignore gaps (not in file)
           for(s32 sector : range(index.sectorCount())) {
-            auto target = _image.data() + 2448ull * (LeadInSectors + lbaDisc + index.lba + sector);
+            auto target = _image.data() + 2448ull * (LeadInSectors + lbaFileBase + index.lba + sector);
             auto length = track.sectorSize();
             if(length == 2048) {
               //ISO: generate header + parity data
               memory::assign(target + 0, 0x00, 0xff, 0xff, 0xff, 0xff, 0xff);  //sync
               memory::assign(target + 6, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00);  //sync
-              auto [minute, second, frame] = CD::MSF(lbaDisc + index.lba + sector);
+              auto [minute, second, frame] = CD::MSF(lbaFileBase + index.lba + sector);
               target[12] = CD::BCD::encode(minute);
               target[13] = CD::BCD::encode(second);
               target[14] = CD::BCD::encode(frame);
@@ -124,9 +149,9 @@ private:
             }
           }
         }
-        offset += track.sectorSize() * track.sectorCount();
+        if(track.postgap) lbaFileBase += track.postgap();
       }
-      lbaDisc += file.tracks.last().indices.last().end + 1;
+      lbaFileBase += file.tracks.last().indices.last().end + 1;
     }
 
     auto subchannel = session.encode(LeadInSectors + session.leadOut.end + 1);
