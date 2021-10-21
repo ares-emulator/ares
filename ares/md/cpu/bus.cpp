7810a05b60f301322843c78298c2ab34259cb829
TascoDLX <tasco.deluxe@gmail.com>
2021-10-21 02:34:06 -0400

    md: improved cpu open bus line accuracy

--- ares/md/cpu/bus.cpp
+++ ares/md/cpu/bus.cpp
@@ -1,6 +1,7 @@
-auto CPU::read(n1 upper, n1 lower, n24 address, n16 data) -> n16 {
+auto CPU::read(n1 upper, n1 lower, n24 address, n16) -> n16 {
   while(bus.acquired()) wait(1);
-  return bus.read(upper, lower, address, data);
+  //using m68k prefetch for open-bus data
+  return bus.read(upper, lower, address, r.irc);
 }
 
 auto CPU::write(n1 upper, n1 lower, n24 address, n16 data) -> void {
