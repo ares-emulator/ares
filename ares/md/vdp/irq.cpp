8b961779afe88e3d388ff3c87824a3f48e4134eb
TascoDLX <tasco.deluxe@gmail.com>
2021-10-22 13:03:47 -0400

    md: fixed irq issues: timing, delays, ack

--- ares/md/vdp/irq.cpp
+++ ares/md/vdp/irq.cpp
@@ -1,32 +1,33 @@
 auto VDP::IRQ::poll() -> void {
+  if(delay) { delay--; return; }
+
   if(external.enable && external.pending) {
-    vdp.debugger.interrupt(CPU::Interrupt::External);
     cpu.raise(CPU::Interrupt::External);
   } else {
-    external.pending = 0;
     cpu.lower(CPU::Interrupt::External);
   }
 
   if(hblank.enable && hblank.pending) {
-    vdp.debugger.interrupt(CPU::Interrupt::HorizontalBlank);
     cpu.raise(CPU::Interrupt::HorizontalBlank);
   } else {
-    // note: pending H-INT is not cleared when disabled
     cpu.lower(CPU::Interrupt::HorizontalBlank);
   }
 
   if(vblank.enable && vblank.pending) {
-    vdp.debugger.interrupt(CPU::Interrupt::VerticalBlank);
     cpu.raise(CPU::Interrupt::VerticalBlank);
   } else {
-    vblank.pending = 0;
     cpu.lower(CPU::Interrupt::VerticalBlank);
   }
 }
 
 auto VDP::IRQ::acknowledge(u8 level) -> void {
   if(level == 2) external.pending = 0;
-  if(level == 4) hblank.pending = 0;
+  if(level == 4) {
+    // H-INT ack can errantly cancel V-INT if they coincide.
+    // Required for Fatal Rewind et al.
+    if(!vblank.pending) hblank.pending = 0;
+    else                vblank.pending = 0;
+  }
   if(level == 6) vblank.pending = 0;
 }
 
@@ -34,4 +35,5 @@ auto VDP::IRQ::power(bool reset) -> void {
   external = {};
   hblank = {};
   vblank = {};
+  delay = 0;
 }
