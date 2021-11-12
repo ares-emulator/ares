2f6edc7efe19336e5471eb17695c4fc0f7078e69
Luke Usher <luke.usher@outlook.com>
2021-11-12 18:22:48 +0000

    msx: add huristics for rom type detection

--- mia/medium/msx.cpp
+++ mia/medium/msx.cpp
@@ -39,15 +39,56 @@ auto MSX::save(string location) -> bool {
 }
 
 auto MSX::analyze(vector<u8>& rom) -> string {
+  string board = "Linear";
+
+  // If the rom is too big to be linear, attempt to guess the mapper
+  // based on the number of times specific banking instructions occur 
+  // in the binary
+  if(rom.size() > 0x10000) {
+    const auto ASC16      = 0;
+    const auto ASC8       = 1;
+    const auto KONAMI     = 2;
+    const auto KONAMI_SCC = 3;
+    int bankingCount[4] = {0,0,0,0}; 
+
+    for(auto i : range(rom.size() - 3)) {
+      if (rom[i] == 0x32) { // ld(nn),a 
+        u16 value = rom[i + 1] + (rom[i + 2] << 8);
+        switch (value) {
+          case 0x5000: case 0x9000: case 0xb000: bankingCount[KONAMI_SCC]++; break;
+          case 0x4000: case 0x8000: case 0xa000: bankingCount[KONAMI]++; break;
+          case 0x6800: case 0x7800: bankingCount[ASC8]++; break;
+          case 0x6000: bankingCount[KONAMI]++; bankingCount[ASC8]++; bankingCount[ASC16]++; break;
+          case 0x7000: bankingCount[KONAMI_SCC]++; bankingCount[ASC8]++; bankingCount[ASC16]++; break;
+          case 0x77ff: bankingCount[ASC16]++; break;
+        }
+      }
+    }
+
+    auto mapperNum = 0, max = 0;
+    for(auto i : range(4)) {
+      if (bankingCount[i] > max) {max = bankingCount[i]; mapperNum = i;}
+    }
+
+    switch(mapperNum) {
+      case 0: board = "ASC16"; break;
+      case 1: board = "ASC8"; break;
+      case 2: board = "Konami"; break;
+      case 3: board = "KonamiSCC"; break; 
+    }
+  }
+
   string s;
   s += "game\n";
   s +={"  name:   ", Medium::name(location), "\n"};
   s +={"  title:  ", Medium::name(location), "\n"};
   s += "  region: NTSC\n";  //database required to detect region
-  s += "  board\n";
+  s +={"  board: ", board, "\n"};
   s += "    memory\n";
   s += "      type: ROM\n";
   s +={"      size: 0x", hex(rom.size()), "\n"};
   s += "      content: Program\n";
+
+  print(s);
   return s;
 }
