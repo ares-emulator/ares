5f6b75a013f540486b7a112f993a079968ca36fc
encoded-byte <90053958+encoded-byte@users.noreply.github.com>
2021-11-18 19:18:58 +0100

    fc: support for cnrom with security

--- ares/fc/cartridge/board/hvc-cnrom.cpp
+++ ares/fc/cartridge/board/hvc-cnrom.cpp
@@ -1,22 +1,25 @@
 struct HVC_CNROM : Interface {
   static auto create(string id) -> Interface* {
-    if(id == "HVC-CNROM") return new HVC_CNROM;
+    if(id == "HVC-CNROM"    ) return new HVC_CNROM(Revision::CNROM);
+    if(id == "HVC-CNROM-SEC") return new HVC_CNROM(Revision::CNROMS);
     return nullptr;
   }
 
   Memory::Readable<n8> programROM;
   Memory::Readable<n8> characterROM;
-  Memory::Writable<n8> characterRAM;
+
+  enum class Revision : u32 {
+    CNROM,
+    CNROMS,
+  } revision;
+
+  HVC_CNROM(Revision revision) : revision(revision) {}
 
   auto load() -> void override {
     Interface::load(programROM, "program.rom");
     Interface::load(characterROM, "character.rom");
-    Interface::load(characterRAM, "character.ram");
     mirror = pak->attribute("mirror") == "vertical";
-  }
-
-  auto save() -> void override {
-    Interface::save(characterRAM, "character.ram");
+    key = pak->attribute("chip/key").natural();
   }
 
   auto readPRG(n32 address, n8 data) -> n8 override {
@@ -27,6 +30,7 @@ struct HVC_CNROM : Interface {
   auto writePRG(n32 address, n8 data) -> void override {
     if(address < 0x8000) return;
     characterBank = data.bit(0,1);
+    characterEnable = (data == key);
   }
 
   auto readCHR(n32 address, n8 data) -> n8 override {
@@ -34,10 +38,12 @@ struct HVC_CNROM : Interface {
       address = address >> !mirror & 0x0400 | (n10)address;
       return ppu.readCIRAM(address);
     }
+    if(revision == Revision::CNROMS) {
+      if(!characterEnable) return 0xff;
+      return characterROM.read((n13)address);
+    }
     address = characterBank << 13 | (n13)address;
-    if(characterROM) return characterROM.read(address);
-    if(characterRAM) return characterRAM.read(address);
-    return data;
+    return characterROM.read(address);
   }
 
   auto writeCHR(n32 address, n8 data) -> void override {
@@ -45,19 +51,17 @@ struct HVC_CNROM : Interface {
       address = address >> !mirror & 0x0400 | (n10)address;
       return ppu.writeCIRAM(address, data);
     }
-    address = characterBank << 13 | (n13)address;
-    if(characterRAM) return characterRAM.write(address, data);
-  }
-
-  auto power() -> void override {
   }
 
   auto serialize(serializer& s) -> void override {
-    s(characterRAM);
     s(mirror);
+    s(key);
     s(characterBank);
+    s(characterEnable);
   }
 
   n1 mirror;  //0 = horizontal, 1 = vertical
+  n8 key;
   n2 characterBank;
+  n1 characterEnable;
 };
