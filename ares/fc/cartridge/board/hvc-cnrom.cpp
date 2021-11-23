fd3a56b0a2f1c767c5c142037a00c8b4f0ef75ae
encoded-byte <90053958+encoded-byte@users.noreply.github.com>
2021-11-23 17:57:14 +0100

    fc: support for cprom board

--- ares/fc/cartridge/board/hvc-cnrom.cpp
+++ ares/fc/cartridge/board/hvc-cnrom.cpp
@@ -2,15 +2,18 @@ struct HVC_CNROM : Interface {
   static auto create(string id) -> Interface* {
     if(id == "HVC-CNROM"    ) return new HVC_CNROM(Revision::CNROM);
     if(id == "HVC-CNROM-SEC") return new HVC_CNROM(Revision::CNROMS);
+    if(id == "HVC-CPROM"    ) return new HVC_CNROM(Revision::CPROM);
     return nullptr;
   }
 
   Memory::Readable<n8> programROM;
   Memory::Readable<n8> characterROM;
+  Memory::Writable<n8> characterRAM;
 
   enum class Revision : u32 {
     CNROM,
     CNROMS,
+    CPROM,
   } revision;
 
   HVC_CNROM(Revision revision) : revision(revision) {}
@@ -18,8 +21,13 @@ struct HVC_CNROM : Interface {
   auto load() -> void override {
     Interface::load(programROM, "program.rom");
     Interface::load(characterROM, "character.rom");
+    Interface::load(characterRAM, "character.ram");
     mirror = pak->attribute("mirror") == "vertical";
     key = pak->attribute("chip/key").natural();
+  }
+
+  auto save() -> void override {
+    Interface::save(characterRAM, "character.ram");
   }
 
   auto readPRG(n32 address, n8 data) -> n8 override {
@@ -38,6 +46,10 @@ struct HVC_CNROM : Interface {
       address = address >> !mirror & 0x0400 | (n10)address;
       return ppu.readCIRAM(address);
     }
+    if(revision == Revision::CPROM) {
+      n2 bank = (address < 0x1000) ? (n2)0 : characterBank;
+      return characterRAM.read(bank << 12 | (n12)address);
+    }
     if(revision == Revision::CNROMS) {
       if(!characterEnable) return 0xff;
       return characterROM.read((n13)address);
@@ -51,9 +63,14 @@ struct HVC_CNROM : Interface {
       address = address >> !mirror & 0x0400 | (n10)address;
       return ppu.writeCIRAM(address, data);
     }
+    if(revision == Revision::CPROM) {
+      n2 bank = (address < 0x1000) ? (n2)0 : characterBank;
+      return characterRAM.write(bank << 12 | (n12)address, data);
+    }
   }
 
   auto serialize(serializer& s) -> void override {
+    s(characterRAM);
     s(mirror);
     s(key);
     s(characterBank);
