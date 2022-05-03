c7e59d13fa1eaa6d6e19daec8dc22a3bdf7e0e70
Luke Usher <luke.usher@outlook.com>
2022-05-03 18:02:47 +0100

    Squashed 'thirdparty/libchdr/' changes from a03e69319..6eeb6abc4
    6eeb6abc4 Merge pull request #76 from invertego/zlib_codec_init_free
    254cdaccc Remove erroneous free() call in zlib_codec_init()
    
    git-subtree-dir: thirdparty/libchdr
    git-subtree-split: 6eeb6abc4adc094d489c8ba8cafdcff9ff61251b

--- src/libchdr_chd.c
+++ src/libchdr_chd.c
@@ -2599,10 +2599,6 @@ static chd_error zlib_codec_init(void *codec, uint32_t hunkbytes)
 	else
 		err = CHDERR_NONE;
 
-	/* handle an error */
-	if (err != CHDERR_NONE)
-		free(data);
-
 	return err;
 }
 
