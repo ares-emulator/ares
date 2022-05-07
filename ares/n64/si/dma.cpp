ares-emulator/ares

commit 741320e0be3a9d53c92408bb4def6d19a232972f

n64: refactor PIF chip into its own class
Currently, Ares does not model the PIF chip on its own: it looks like
there was an original mistake that conflated the PI (peripheral
interface, one of the components of RCP) with the PIF-NUS (a separate
chip, connected to SI component of RCP via a serial bus).

This refactor introduces a PIF class and moves all the related code
into it. Since the class implements the Memory::IO protocol for memory
accesses, it also fixes memory accesses with non standard sizes, as
tested by n64-systemtest.

ares/n64/si/dma.cpp
@@ -1,5 +1,5 @@
 auto SI::dmaRead() -> void {
-  run();
+  pif.run();
   for(u32 offset = 0; offset < 64; offset += 2) {
     u16 data = bus.read<Half>(io.readAddress + offset);
     bus.write<Half>(io.dramAddress + offset, data);
@@ -17,5 +17,5 @@ auto SI::dmaWrite() -> void {
   io.dmaBusy = 0;
   io.interrupt = 1;
   mi.raise(MI::IRQ::SI);
-  run();
+  pif.run();
 }

ares/n64/GNUmakefile
@@ -9,6 +9,7 @@ ares.objects += ares-n64-mi
 ares.objects += ares-n64-vi
 ares.objects += ares-n64-ai
 ares.objects += ares-n64-pi
+ares.objects += ares-n64-pif
 ares.objects += ares-n64-ri
 ares.objects += ares-n64-si
 ares.objects += ares-n64-rdram
@@ -27,6 +28,7 @@ $(object.path)/ares-n64-mi.o:         $(ares.path)/n64/mi/mi.cpp
 $(object.path)/ares-n64-vi.o:         $(ares.path)/n64/vi/vi.cpp
 $(object.path)/ares-n64-ai.o:         $(ares.path)/n64/ai/ai.cpp
 $(object.path)/ares-n64-pi.o:         $(ares.path)/n64/pi/pi.cpp
+$(object.path)/ares-n64-pif.o:        $(ares.path)/n64/pif/pif.cpp
 $(object.path)/ares-n64-ri.o:         $(ares.path)/n64/ri/ri.cpp
 $(object.path)/ares-n64-si.o:         $(ares.path)/n64/si/si.cpp
 $(object.path)/ares-n64-rdram.o:      $(ares.path)/n64/rdram/rdram.cpp

ares/n64/memory/bus.hpp
@@ -18,11 +18,7 @@ inline auto Bus::read(u32 address) -> u64 {
   if(address <= 0x048f'ffff) return si.read<Size>(address);
   if(address <= 0x04ff'ffff) return unmapped;
   if(address <= 0x1fbf'ffff) return pi.read<Size>(address);
-  if(address <= 0x1fc0'07bf) {
-    if(pi.io.romLockout) return unmapped;
-    return pi.rom.read<Size>(address);
-  }
-  if(address <= 0x1fc0'07ff) return pi.ram.read<Size>(address);
+  if(address <= 0x1fcf'ffff) return pif.read<Size>(address);
   return unmapped;
 }
 
@@ -49,10 +45,6 @@ inline auto Bus::write(u32 address, u64 data) -> void {
   if(address <= 0x048f'ffff) return si.write<Size>(address, data);
   if(address <= 0x04ff'ffff) return;
   if(address <= 0x1fbf'ffff) return pi.write<Size>(address, data);
-  if(address <= 0x1fc0'07bf) {
-    if(pi.io.romLockout) return;
-    return pi.rom.write<Size>(address, data);
-  }
-  if(address <= 0x1fc0'07ff) return pi.ram.write<Size>(address, data);
+  if(address <= 0x1fcf'ffff) return pif.write<Size>(address, data);
   return;
 }

ares/n64/n64.hpp
@@ -66,6 +66,7 @@ namespace ares::Nintendo64 {
   #include <n64/vi/vi.hpp>
   #include <n64/ai/ai.hpp>
   #include <n64/pi/pi.hpp>
+  #include <n64/pif/pif.hpp>
   #include <n64/ri/ri.hpp>
   #include <n64/si/si.hpp>
   #include <n64/rdram/rdram.hpp>

ares/n64/pi/debugger.cpp
@@ -1,13 +1,4 @@
 auto PI::Debugger::load(Node::Object parent) -> void {
-  memory.ram = parent->append<Node::Debugger::Memory>("PI RAM");
-  memory.ram->setSize(64);
-  memory.ram->setRead([&](u32 address) -> u8 {
-    return pi.ram.read<Byte>(address);
-  });
-  memory.ram->setWrite([&](u32 address, u8 data) -> void {
-    return pi.ram.write<Byte>(address, data);
-  });
-
   tracer.io = parent->append<Node::Debugger::Tracer::Notification>("I/O", "PI");
 }
 

ares/n64/pi/pi.cpp
@@ -10,47 +10,19 @@ PI pi;
 
 auto PI::load(Node::Object parent) -> void {
   node = parent->append<Node::Object>("PI");
-  rom.allocate(0x7c0);
-  ram.allocate(0x040);
 
   debugger.load(node);
 }
 
 auto PI::unload() -> void {
   debugger = {};
-  rom.reset();
-  ram.reset();
   node.reset();
 }
 
 auto PI::power(bool reset) -> void {
-  string pifrom = cartridge.region() == "NTSC" ? "pif.ntsc.rom" : "pif.pal.rom";
-  if(auto fp = system.pak->read(pifrom)) {
-    rom.load(fp);
-  }
-
-  ram.fill();
   io = {};
   bsd1 = {};
   bsd2 = {};
-
-  //write CIC seeds into PIF RAM so that cartridge checksum function passes
-  string cic = cartridge.cic();
-  n8 seed = 0x3f;
-  n1 version = 0;
-  if(cic == "CIC-NUS-6101" || cic == "CIC-NUS-7102") seed = 0x3f, version = 1;
-  if(cic == "CIC-NUS-6102" || cic == "CIC-NUS-7101") seed = 0x3f;
-  if(cic == "CIC-NUS-6103" || cic == "CIC-NUS-7103") seed = 0x78;
-  if(cic == "CIC-NUS-6105" || cic == "CIC-NUS-7105") seed = 0x91;
-  if(cic == "CIC-NUS-6106" || cic == "CIC-NUS-7106") seed = 0x85;
-
-  n32 data;
-  data.bit(0, 7) = 0x3f;     //CIC IPL2 seed
-  data.bit(8,15) = seed;     //CIC IPL3 seed
-  data.bit(17)   = reset;    //osResetType (0 = power; 1 = reset (NMI))
-  data.bit(18)   = version;  //osVersion
-  data.bit(19)   = 0;        //osRomType (0 = Gamepak; 1 = 64DD)
-  ram.write<Word>(0x24, data);
 }
 
 }

ares/n64/pi/pi.hpp
@@ -2,17 +2,11 @@
 
 struct PI : Memory::IO<PI> {
   Node::Object node;
-  Memory::Readable rom;
-  Memory::Writable ram;
 
   struct Debugger {
     //debugger.cpp
     auto load(Node::Object) -> void;
     auto io(bool mode, u32 address, u32 data) -> void;
-
-    struct Memory {
-      Node::Debugger::Memory ram;
-    } memory;
 
     struct Tracer {
       Node::Debugger::Tracer::Notification io;
@@ -54,7 +48,6 @@ struct PI : Memory::IO<PI> {
     n32 pbusAddress;
     n32 readLength;
     n32 writeLength;
-    n1  romLockout;
     n32 busLatch;
   } io;
 

ares/n64/pi/serialization.cpp
@@ -1,6 +1,4 @@
 auto PI::serialize(serializer& s) -> void {
-  s(ram);
-
   s(io.dmaBusy);
   s(io.ioBusy);
   s(io.error);
@@ -9,7 +7,6 @@ auto PI::serialize(serializer& s) -> void {
   s(io.pbusAddress);
   s(io.readLength);
   s(io.writeLength);
-  s(io.romLockout);
   s(io.busLatch);
 
   s(bsd1.latency);

commit 673fd40bacd33c0280e6411727397c98a46d8bfd

n64: fix controller input after PIF refactoring
A last minute change in 741320e0be introduced a regression in controller
input.

Similar to what we did with PI DMA, SI DMA does not go through the main
bus (SysAD bus) but directly communicate with pif and rdram. This is not
just for the sake of correctness, but SysAD bus half-word writes are
now emulated with the same brokeness they have on real hardware, and
obviously DMA works instead. Moreover, since SI only reads/writes
32-bit words, also switch to word access.

ares/n64/si/dma.cpp
@@ -1,8 +1,8 @@
 auto SI::dmaRead() -> void {
   pif.run();
-  for(u32 offset = 0; offset < 64; offset += 2) {
-    u16 data = bus.read<Half>(io.readAddress + offset);
-    bus.write<Half>(io.dramAddress + offset, data);
+  for(u32 offset = 0; offset < 64; offset += 4) {
+    u32 data = pif.readWord(io.readAddress + offset);
+    rdram.ram.write<Word>(io.dramAddress + offset, data);
   }
   io.dmaBusy = 0;
   io.interrupt = 1;
@@ -10,9 +10,9 @@ auto SI::dmaRead() -> void {
 }
 
 auto SI::dmaWrite() -> void {
-  for(u32 offset = 0; offset < 64; offset += 2) {
-    u16 data = bus.read<Half>(io.dramAddress + offset);
-    bus.write<Half>(io.writeAddress + offset, data);
+  for(u32 offset = 0; offset < 64; offset += 4) {
+    u32 data = rdram.ram.read<Word>(io.dramAddress + offset);
+    pif.writeWord(io.writeAddress + offset, data);
   }
   io.dmaBusy = 0;
   io.interrupt = 1;