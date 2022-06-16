99b759b630d6026ab22ec71b785ef78cb30b8040
Giovanni Bajo <rasky@develer.com>
2022-06-16 13:20:28 +0200

    n64: improve 64-bit TLB exceptions
    It looks like address exceptions and TLB exceptions always set the
    same registers (though some are marked as undefined in the manual), so
    we unify the two implementations.
    
    Fix several tests in n64-systemtest.

--- ares/n64/cpu/tlb.cpp
+++ ares/n64/cpu/tlb.cpp
@@ -1,12 +1,11 @@
-//the N64 TLB is 32-bit only: only the 64-bit XTLB exception vector is used.
 
-auto CPU::TLB::load(u32 vaddr) -> Match {
+auto CPU::TLB::load(u64 vaddr) -> Match {
   for(auto& entry : this->entry) {
     if(!entry.globals && entry.addressSpaceID != self.scc.tlb.addressSpaceID) continue;
     if((u32)(vaddr & entry.addressMaskHi) != (u32)entry.virtualAddress) continue;
     bool lo = vaddr & entry.addressSelect;
     if(!entry.valid[lo]) {
-      exception(vaddr);
+      self.addressException(vaddr);
       self.debugger.tlbLoadInvalid(vaddr);
       self.exception.tlbLoadInvalid();
       return {false};
@@ -15,25 +14,25 @@ auto CPU::TLB::load(u32 vaddr) -> Match {
     self.debugger.tlbLoad(vaddr, physicalAddress);
     return {true, entry.cacheAlgorithm[lo] != 2, physicalAddress};
   }
-  exception(vaddr);
+  self.addressException(vaddr);
   self.debugger.tlbLoadMiss(vaddr);
   self.exception.tlbLoadMiss();
   return {false};
 }
 
-auto CPU::TLB::store(u32 vaddr) -> Match {
+auto CPU::TLB::store(u64 vaddr) -> Match {
   for(auto& entry : this->entry) {
     if(!entry.globals && entry.addressSpaceID != self.scc.tlb.addressSpaceID) continue;
     if((u32)(vaddr & entry.addressMaskHi) != (u32)entry.virtualAddress) continue;
     bool lo = vaddr & entry.addressSelect;
     if(!entry.valid[lo]) {
-      exception(vaddr);
+      self.addressException(vaddr);
       self.debugger.tlbStoreInvalid(vaddr);
       self.exception.tlbStoreInvalid();
       return {false};
     }
     if(!entry.dirty[lo]) {
-      exception(vaddr);
+      self.addressException(vaddr);
       self.debugger.tlbModification(vaddr);
       self.exception.tlbModification();
       return {false};
@@ -42,18 +41,10 @@ auto CPU::TLB::store(u32 vaddr) -> Match {
     self.debugger.tlbStore(vaddr, physicalAddress);
     return {true, entry.cacheAlgorithm[lo] != 2, physicalAddress};
   }
-  exception(vaddr);
+  self.addressException(vaddr);
   self.debugger.tlbStoreMiss(vaddr);
   self.exception.tlbStoreMiss();
   return {false};
-}
-
-auto CPU::TLB::exception(u32 vaddr) -> void {
-  self.scc.badVirtualAddress = vaddr;
-  self.scc.tlb.virtualAddress.bit(13,39) = vaddr >> 13;
-  self.scc.context.badVirtualAddress = vaddr >> 13;
-  self.scc.xcontext.badVirtualAddress = vaddr >> 13;
-  self.scc.xcontext.region = 0;
 }
 
 auto CPU::TLB::Entry::synchronize() -> void {
