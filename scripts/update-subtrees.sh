cbdedc9f9ce3ae0fb7202e85c9e31c72ebbb2653
Giovanni Bajo <rasky@develer.com>
2022-09-23 12:25:24 +0200

    Move update-subtrees.sh into scripts subdirectory

--- scripts/update-subtrees.sh
+++ scripts/update-subtrees.sh
@@ -5,9 +5,9 @@
 # If you don't have a POSIX-compatible shell on your system, feel free to use
 # this as a reference for what commands to run, rather than running it directly.
 
-# Change to the directory containing this script, or exit with failure
+# Change to the root directory, or exit with failure
 # to prevent git subtree scrawling over some other repo.
-cd "$(dirname "$0")" || exit 1
+cd "$(dirname "$0")"/.. || exit 1
 
 # Merge changes from the upstream parallel-rdp repository.
 git subtree pull --prefix=ares/n64/vulkan/parallel-rdp https://github.com/Themaister/parallel-rdp-standalone.git master --squash
