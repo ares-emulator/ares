e9cb63508e99f9eaf769a7afe029c754351fd327
Giovanni Bajo <rasky@develer.com>
2022-09-23 12:24:34 +0200

    Add script to build an universal binary on mac

---
+++ scripts/macos-make-universal.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euo pipefail
+
+if ! command -v lipo >/dev/null; then
+    echo "Command lipo not found; please install XCode"
+    exit 1
+fi
+
+if ! command -v gmake >/dev/null; then
+    echo "Please install make via Homebrew (brew install make)"
+    exit 1
+fi
+
+# Change to parent directory (top-level)
+cd "$(dirname "$0")"/.. || exit 1
+
+echo "Building for amd64..."
+gmake arch=amd64 object.path=obj-amd64 output.path=out-amd64 "$@"
+
+echo "Building for arm64..."
+gmake arch=arm64 object.path=obj-arm64 output.path=out-arm64 "$@"
+
+echo "Assembling universal binary"
+rm -rf desktop-ui/out
+cp -a desktop-ui/out-amd64 desktop-ui/out
+lipo -create -output desktop-ui/out/ares.app/Contents/MacOS/ares \
+    desktop-ui/out-amd64/ares.app/Contents/MacOS/ares \
+    desktop-ui/out-arm64/ares.app/Contents/MacOS/ares
+codesign --force --deep --sign - desktop-ui/out/ares.app
