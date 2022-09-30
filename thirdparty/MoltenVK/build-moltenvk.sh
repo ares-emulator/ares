a238a7e7547d2d36f69b8ac709b63ce7afb875fa
Giovanni Bajo <rasky@develer.com>
2022-09-30 12:51:06 +0200

    ci: cache MoltenVK builds to speed up CI

--- thirdparty/MoltenVK/build-moltenvk.sh
+++ thirdparty/MoltenVK/build-moltenvk.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-if [ -d "MoltenVK" ]; then
-	git -C MoltenVK pull
-else
+if [ ! -d "MoltenVK" ]; then
 	git clone https://github.com/KhronosGroup/MoltenVK.git
+else
+	git -C MoltenVK fetch
 fi
+git -C MoltenVK reset --hard "$(cat HEAD)"
 pushd MoltenVK
 ./fetchDependencies --macos
 make macos MVK_LOG_LEVEL_INFO=0 MVK_LOG_LEVEL_DEBUG=0
 popd
 cp MoltenVK/MoltenVK/dylib/macOS/libMoltenVK.dylib .
-git -C MoltenVK rev-parse HEAD >HEAD
