8a71af4be1883f13df10fd0e76d4d1678afd1954
Luke Usher <luke@usher.cc>
2023-01-20 13:37:37 +0000

    n64: fix is-viewer debug output in commercial games + libdragon
    This is a little bit hacky, because the debugging code in libdragon
    is not compatible with actual IS-Viewer hardware, so we emulate
    a 'quirky' version that satisfies both libraries.

--- ares/n64/cartridge/isviewer.cpp
+++ ares/n64/cartridge/isviewer.cpp
@@ -1,22 +1,28 @@
 auto Cartridge::ISViewer::readWord(u32 address) -> u32 {
+  address = (address & 0xffff);
   u32 data = ram.read<Word>(address);
-  address = (address & 0xffff) >> 2;
-
-  if(address == 0) {
-    data = 0x49533634;  //'IS64'
-  }
-
   return data;
 }
 
 auto Cartridge::ISViewer::writeWord(u32 address, u32 data) -> void {
-  ram.write<Word>(address, data);
-  address = (address & 0xffff) >> 2;
+  address = (address & 0xffff);
 
-  if(address == 5) {
+  if(address == 0x14) {
+    // HACK: allow printf output to work for both libultra and libdragon
+    // Libultra expects a real IS-Viewer device and treats this address as a
+    // pointer to the end of the buffer, reading the current value, writing N
+    // bytes, then updating the buffer pointer.
+    // libdragon instead treats this as a "number of bytes" register, only
+    // writing an "output byte count"
+    // In order to satisfy both libraries, we assume it behaves as libdragon
+    // expects, and by forcing the write to never hit ram, libultra remains
+    // functional.
     for(auto address : range(u16(data))) {
       char c = ram.read<Byte>(0x20 + address);
       fputc(c, stdout);
     }
+    return;
   }
+
+  ram.write<Word>(address, data);
 }
