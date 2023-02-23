4b70e4e8919ffbe7324e3d2d2d725d262543cd89
invertego <invertego@users.noreply.github.com>
2023-02-22 19:34:56 -0800

    mcd/32x: default to single gamepad for Corpse Killer (E)

--- desktop-ui/emulator/mega-cd-32x.cpp
+++ desktop-ui/emulator/mega-cd-32x.cpp
@@ -66,8 +66,9 @@ auto MegaCD32X::load() -> bool {
   }
 
   if(auto port = root->find<ares::Node::Port>("Controller Port 2")) {
-    if(game->pak->attribute("serial") == "GM T-16201F-00") {
+    if(game->pak->attribute("serial").beginsWith("GM T-16201F")) {
       // CORPSE KILLER 32X (U) -- GM T-16201F-00
+      // CORPSE KILLER 32X (E) -- GM T-16201F-50
       // Gamepad in controller port 2 breaks input polling, so leave it disconnected.
       // No supported lightgun devices are currently emulated (Sega Menacer, ALG GameGun).
     } else {
