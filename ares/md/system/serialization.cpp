7e237528b4c5499497d0fd164aa2b1d0214be9d4
TascoDLX <tasco.deluxe@gmail.com>
2023-03-27 00:56:45 -0400

    md: bump serializer version

--- ares/md/system/serialization.cpp
+++ ares/md/system/serialization.cpp
@@ -1,4 +1,4 @@
-static const string SerializerVersion = "v132.1";
+static const string SerializerVersion = "v132.2";
 
 auto System::serialize(bool synchronize) -> serializer {
   if(synchronize) scheduler.enter(Scheduler::Mode::Synchronize);
