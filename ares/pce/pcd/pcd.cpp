21c24b31ad6ee1d3d40f0ffdd751317cf83188d8
invertego <invertego@users.noreply.github.com>
2023-04-08 22:21:01 -0700

    pce: prevent destruction of bram on non-CD load
    With PC Engine, ares always emulates the presence of CD-ROM drive so
    games can use its backup RAM for save data. However, it was not properly
    loading the backup RAM file when loading a non-CD game, causing the file
    to be subsequently filled with FFs on save.

--- ares/pce/pcd/pcd.cpp
+++ ares/pce/pcd/pcd.cpp
@@ -45,6 +45,25 @@ auto PCD::load(Node::Object parent) -> void {
   cdda.load(node);
   adpcm.load(node);
   debugger.load(node);
+
+  if(auto fp = system.pak->read("backup.ram")) {
+    bram.load(fp);
+  }
+
+  // Initialise bram to prevent 'rma error' screens in many games
+  // Only do this if the HUBM string is not present, to prevent overwriting
+  // existing bram
+  if (bram[0] != 'H' && bram[1] != 'U' && bram[2] != 'B' && bram[3] != 'M') {
+    bram.fill(0);
+    bram[0] = 'H';
+    bram[1] = 'U';
+    bram[2] = 'B';
+    bram[3] = 'M';
+    bram[4] = 0x00;
+    bram[5] = 0x88;
+    bram[6] = 0x10;
+    bram[7] = 0x80;
+  }
 }
 
 auto PCD::unload() -> void {
@@ -88,25 +107,6 @@ auto PCD::connect() -> void {
     fd->read({subchannel.data() + sector * 96, 96});
   }
   session.decode(subchannel, 96);
-
-  if(auto fp = system.pak->read("backup.ram")) {
-    bram.load(fp);
-  }
-
-  // Initialise bram to prevent 'rma error' screens in many games
-  // Only do this if the HUBM string is not present, to prevent overwriting
-  // existing bram
-  if (bram[0] != 'H' && bram[1] != 'U' && bram[2] != 'B' && bram[3] != 'M') {
-    bram.fill(0);
-    bram[0] = 'H';
-    bram[1] = 'U';
-    bram[2] = 'B';
-    bram[3] = 'M';
-    bram[4] = 0x00;
-    bram[5] = 0x88;
-    bram[6] = 0x10;
-    bram[7] = 0x80;
-  }
 }
 
 auto PCD::disconnect() -> void {
