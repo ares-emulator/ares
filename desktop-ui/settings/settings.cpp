47665591de037423e2eb8f3506bdc44e755d817b
hackgrid <hackgrid@users.noreply.github.com>
2023-08-04 10:55:31 +0200

    desktop-ui: save settings when closing the settings dialogue

--- desktop-ui/settings/settings.cpp
+++ desktop-ui/settings/settings.cpp
@@ -155,6 +155,7 @@ auto Settings::process(bool load) -> void {
 
 SettingsWindow::SettingsWindow() {
   onClose([&] {
+    save();
     setVisible(false);
     //cancel any pending input assignment requests, if any
     inputSettings.setVisible(false);
