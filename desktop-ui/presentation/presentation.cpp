c6cd1322eadb1212d23893a05b19d23ba4f38066
hackgrid <hackgrid@users.noreply.github.com>
2023-08-10 14:27:46 +0200

    desktop-ui: add fast load option to load menu

--- desktop-ui/presentation/presentation.cpp
+++ desktop-ui/presentation/presentation.cpp
@@ -374,7 +374,24 @@ auto Presentation::loadEmulators() -> void {
     }
   }
   loadMenu.append(MenuSeparator());
+  
+  { MenuItem loadFastAction{&loadMenu};
+    loadFastAction.setText("Load fast" ELLIPSIS).setIcon(Icon::Emblem::Folder).onActivate([&] {  
+      string location;
+      BrowserDialog dialog;
+      dialog.setTitle({"Load Game"});
+      dialog.setPath(Path::desktop());
+      dialog.setAlignment(presentation);
+      dialog.setFilters({"All|*"});
 
+      if(auto location = program.openFile(dialog)) {
+        if(auto emulator = program.identify(location))
+          program.load(emulator, location);
+      }
+    });  
+  }
+  loadMenu.append(MenuSeparator());  
+  
   //build emulator load list
   for(auto& emulator : emulators) {
     MenuItem item;
