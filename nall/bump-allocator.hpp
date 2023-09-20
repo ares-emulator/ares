363df3706782dde98c736e0fa3605d7bc9c289a8
invertego <invertego@users.noreply.github.com>
2023-09-20 02:52:41 -0700

    recompiler: avoid overallocation in code buffer
    sljit performs a single (potentially) oversized executable memory
    allocation before generating code. Instead of reserving the requested
    amount of space, we can reserve only the used amount and eliminate some
    dead space between code blocks.

--- nall/bump-allocator.hpp
+++ nall/bump-allocator.hpp
@@ -88,9 +88,9 @@ struct bump_allocator {
     _offset = nextOffset(size);  //alignment
   }
 
-  auto tryAcquire(u32 size) -> u8* {
+  auto tryAcquire(u32 size, bool reserve = true) -> u8* {
     if((nextOffset(size)) > _capacity) return nullptr;
-    return acquire(size);
+    return reserve ? acquire(size) : acquire();
   }
 
 private:
