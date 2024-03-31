1c876d8bf61bdad98bc5e6dbff95ca2edbf1cc14
Luke Usher <luke@usher.cc>
2024-03-31 11:24:36 +0100

    md: fix 32x dotshift in mode 1

--- ares/md/m32x/vdp.cpp
+++ ares/md/m32x/vdp.cpp
@@ -37,8 +37,8 @@ auto M32X::VDP::scanline(u32 pixels[1280], u32 y) -> void {
 
 auto M32X::VDP::scanlineMode1(u32 pixels[1280], u32 y) -> void {
   u16 address = fbram[y];
-  for(u32 x = dotshift; x < 320 + dotshift; x++) {
-    u8 color = fbram[address + (x >> 1) & 0xffff].byte(!(x & 1));
+  for(u32 x : range(320)) {
+    u8 color = fbram[address + (x + dotshift >> 1) & 0xffff].byte(!(x + dotshift & 1));
     plot(&pixels[x * 4], cram[color]);
   }
 }
