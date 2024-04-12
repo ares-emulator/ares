dd7fd4ea52a5a1dd40a80abb95b05335a1b0b175
Luke Usher <luke@usher.cc>
2024-04-12 14:54:01 +0100

    mia: load sfc boards from filesystem rather than embedding in mia

--- mia/system/super-famicom.cpp
+++ mia/system/super-famicom.cpp
@@ -8,7 +8,7 @@ auto SuperFamicom::load(string location) -> bool {
   this->location = locate();
   pak = new vfs::directory;
   pak->append("ipl.rom", Resource::SuperFamicom::IPLROM);
-  pak->append("boards.bml", Resource::SuperFamicom::Boards);
+  pak->append("boards.bml", file::read(mia::locate("Database/Super Famicom Boards.bml")));
   return true;
 }
 
