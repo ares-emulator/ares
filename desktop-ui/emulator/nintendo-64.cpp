ares-emulator/ares

commit 7ad14ff6bfbd7a67fa6670101ef8a30bfa030e5e

n64: fix savedata duplication issue
This fixes an issue where upon loading a game that doesn't use a controller pak after a game that does, a .pak file would be created using the previous games data.

desktop-ui/emulator/nintendo-64.cpp
@@ -193,6 +193,10 @@ auto Nintendo64::load(Menu menu) -> void {
 }
 
 auto Nintendo64::unload() -> void {
+  gamepad.reset();
+  disk.reset();
+  gb.reset();
+
   Emulator::unload();
   diskInsertTimer.reset();
 }

desktop-ui/emulator/nintendo-64dd.cpp
@@ -137,6 +137,8 @@ auto Nintendo64DD::load(Menu menu) -> void {
 }
 
 auto Nintendo64DD::unload() -> void {
+  gamepad.reset();
+
   Emulator::unload();
   diskInsertTimer.reset();
 }

commit 1b3e826575e05c45cfa0044b8a31c8cec86c37a1

n64: Unload and save before reset() on gamepad and disk (#1464)
https://github.com/ares-emulator/ares/commit/7ad14ff6bfbd7a67fa6670101ef8a30bfa030e5e
inadvertently made it so that .pak saves (and other save data) are not
properly persisted to disk when unloading the n64 core. This PR resolves
that, calling `unload()`, which itself calls `save()`, before resetting
the gamepad and other memory.

Co-authored-by: jcm <butt@butts.com>

desktop-ui/emulator/nintendo-64.cpp
@@ -193,11 +193,12 @@ auto Nintendo64::load(Menu menu) -> void {
 }
 
 auto Nintendo64::unload() -> void {
+  Emulator::unload();
+
   gamepad.reset();
   disk.reset();
   gb.reset();
 
-  Emulator::unload();
   diskInsertTimer.reset();
 }
 