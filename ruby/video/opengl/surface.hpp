f42aa1223c4e7a31b66d5a781f2a4b690c9fe0f6
jcm <butt@butts.com>
2024-06-21 00:05:34 -0500

    ruby: Use cleaner OpenGL render pattern, misc. CGL fixes

--- ruby/video/opengl/surface.hpp
+++ ruby/video/opengl/surface.hpp
@@ -24,7 +24,7 @@ auto OpenGLSurface::render(u32 sourceWidth, u32 sourceHeight, u32 targetX, u32 t
 
   if(_chain != NULL) {
     libra_source_image_gl_t input = {texture, format, sourceWidth, sourceHeight};
-    libra_viewport_t viewport{(float)targetX, (float)targetY, targetWidth, targetHeight};
+    libra_viewport_t viewport{0, 0, targetWidth, targetHeight};
     libra_output_framebuffer_gl_t output = {framebuffer, framebufferTexture, framebufferFormat};
 
     if (auto error = _libra.gl_filter_chain_frame(&_chain, frameCount++, input, viewport, output, NULL, NULL)) {
@@ -33,7 +33,7 @@ auto OpenGLSurface::render(u32 sourceWidth, u32 sourceHeight, u32 targetX, u32 t
 
     glBindFramebuffer(GL_READ_FRAMEBUFFER, framebuffer);
     glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
-    glBlitFramebuffer(0, framebufferHeight, framebufferWidth, 0, 0, 0, framebufferWidth, framebufferHeight, GL_COLOR_BUFFER_BIT, filter);
+    glBlitFramebuffer(0, framebufferHeight, framebufferWidth, 0, targetX, targetY, framebufferWidth + targetX, framebufferHeight + targetY, GL_COLOR_BUFFER_BIT, filter);
     glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
   } else {
     glBindFramebuffer(GL_READ_FRAMEBUFFER, framebuffer);
