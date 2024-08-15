53e9aa247dd91c863022f6dcf235e2c0fba6bef1
Giovanni Bajo <rasky@develer.com>
2024-08-15 02:35:20 +0200

    n64: add support for multi-byte writes to gdb
    Currently, gdb hooks in n64 doesn't allow for large sizes of writes
    because it wouldn't be obvious which data size use on the bus.
    While this is true, it doesn't really matter for RDRAM where all data
    sizes work in the obvious, correct way. So relax the constraint to
    allow writing large portions of RDRAM with a single gdb command.
    
    Co-Authored-By: Chase Meadors <c.ed.mead@gmail.com>

--- nall/gdb/server.hpp
+++ nall/gdb/server.hpp
@@ -48,7 +48,7 @@ class Server : public nall::TCPText::Server {
     struct {
       // Memory
       function<string(u64 address, u32 byteCount)> read{};
-      function<void(u64 address, u32 unitSize, u64 value)> write{};
+      function<void(u64 address, vector<u8> value)> write{};
       function<u64(u64 address)> normalizeAddress{};
 
       // Registers
