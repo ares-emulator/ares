9fa916262ec481695d671c5d631c889d352fdd38
jcm <6864788+jcm93@users.noreply.github.com>
2024-11-17 19:43:18 +0000

    nall: Use relative path for prefix

--- nall/stringize.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// The # preprocessing operator converts the argument into a quoted string literal.
-// In order to use this for macro parameters which need to be expanded, this must
-// be a two-level-macro call.
-
-#define stringize_(arg) #arg
-#define stringize(arg) stringize_(arg)
