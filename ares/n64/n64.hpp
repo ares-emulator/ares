0effd4616c6afbe18417052f86b98a78fc603aa1
Giovanni Bajo <rasky@develer.com>
2025-01-05 00:16:24 +0100

    Update sse2neon.h (af7596e)

--- ares/n64/n64.hpp
+++ ares/n64/n64.hpp
@@ -15,6 +15,7 @@
 #include <nmmintrin.h>
 using v128 = __m128i;
 #elif defined(ARCHITECTURE_ARM64) && !defined(COMPILER_MICROSOFT)
+#define SSE2NEON_SUPPRESS_WARNINGS
 #include <sse2neon.h>
 using v128 = __m128i;
 #endif
