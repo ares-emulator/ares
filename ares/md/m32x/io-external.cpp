bb18af05af699a838855ea832d4ccf98ba928259
TascoDLX <tasco.deluxe@gmail.com>
2025-02-02 04:02:03 -0500

    m32x: io fixups

--- ares/md/m32x/io-external.cpp
+++ ares/md/m32x/io-external.cpp
@@ -27,6 +27,7 @@ auto M32X::readExternalIO(n1 upper, n1 lower, n24 address, n16 data) -> n16 {
   //bank set
   if(address == 0xa15104) {
     data.bit(0,1) = io.romBank;
+    data.bit(2, 15) = 0;
   }
 
   //data request control
@@ -193,12 +194,17 @@ auto M32X::writeExternalIO(n1 upper, n1 lower, n24 address, n16 data) -> void {
     if(lower) {
       dreq.vram   = data.bit(0);
       dreq.dma    = data.bit(1);
-      dreq.active = data.bit(2);
-      if(!dreq.active) {
+      if(dreq.active > data.bit(2)) {
+        // Night Trap 32X: delay between the last fifo write and flush on disable
+        if(cpu.active()) cpu.wait(1);
+        // Note: The current fifo implementation is incorrect (hw uses dual-block fifo)
+        // but dreq dma works fine as is for normal cases.
+
         dreq.fifo.flush();
         shm.dmac.dreq[0] = 0;
         shs.dmac.dreq[0] = 0;
       }
+      dreq.active = data.bit(2);
     }
   }
 
@@ -228,11 +234,14 @@ auto M32X::writeExternalIO(n1 upper, n1 lower, n24 address, n16 data) -> void {
 
   //FIFO
   if(address == 0xa15112) {
+    if(!lower || !upper) debug(unusual, "[DREQ] fifo byte write ", lower ? "(odd)" : "(even)");
     if(dreq.active && !dreq.fifo.full()) {
       dreq.fifo.write(data);
       if(!--dreq.length) dreq.active = 0;
       shm.dmac.dreq[0] = !dreq.fifo.empty();
       shs.dmac.dreq[0] = !dreq.fifo.empty();
+    } else {
+      debug(unusual, "[DREQ] missed fifo write");
     }
   }
 
