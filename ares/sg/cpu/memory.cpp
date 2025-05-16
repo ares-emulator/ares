f4f48eb4b9739c5853c85b33b36d632063962fa7
Luke Usher <luke@usher.cc>
2025-05-16 14:52:22 +0100

    sg: fix that base SG-1000 doesn't use a PPI
    The PPI chip is only present in SG-1000A (Arcade Board) and SC-3000

--- ares/sg/cpu/memory.cpp
+++ ares/sg/cpu/memory.cpp
@@ -18,43 +18,36 @@ auto CPU::write(n16 address, n8 data) -> void {
 }
 
 auto CPU::in(n16 address) -> n8 {
+  n8 output = 0xff;
+
   switch(address.bit(6,7)) {
 
-  case 0: {
-    return 0xff;
-  }
+  case 2:
+    return output = !address.bit(0) ? vdp.data() : vdp.status();
+  case 3:
+    if(Model::SG1000()) {
+      if(address.bit(0) == 0) {
+        output.bit(0, 5) = controllerPort1.read();
+        output.bit(6, 7) = controllerPort2.read().bit(0, 1);
+      } else {
+        output.bit(0, 3) = controllerPort2.read().bit(2, 5);
+      }
 
-  case 1: {
-    return 0xff;
-  }
+      return output;
+    }
 
-  case 2: {
-    return !address.bit(0) ? vdp.data() : vdp.status();
-  }
-
-  case 3: {
     return ppi.read(address.bit(0,1));
   }
 
-  }
-
-  return 0xff;
+  return output;
 }
 
 auto CPU::out(n16 address, n8 data) -> void {
   switch(address.bit(6,7)) {
 
-  case 1: {
-    return psg.write(data);
-  }
-
-  case 2: {
-    return !address.bit(0) ? vdp.data(data) : vdp.control(data);
-  }
-
-  case 3: {
-    return ppi.write(address.bit(0,1), data);
-  }
+  case 1: return psg.write(data);
+  case 2: return !address.bit(0) ? vdp.data(data) : vdp.control(data);
+  case 3: if(!Model::SG1000()) return ppi.write(address.bit(0,1), data); break;
 
   }
 }
