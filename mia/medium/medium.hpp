2950f59acd442aac88e3ae706089a53b1400f98b
Roger Sanders <sanders_roger@hotmail.com>
2025-08-12 09:05:37 +1000

    Fixed another merge error

--- mia/medium/medium.hpp
+++ mia/medium/medium.hpp
@@ -35,6 +35,7 @@ private:
   auto readDataSectorCUE(string filename, u32 sectorID) -> vector<u8>;
 #if defined(ARES_ENABLE_CHD)
   auto readDataSectorCHD(string filename, u32 sectorID) -> vector<u8>;
+#endif
 };
 
 struct FloppyDisk : Medium {
