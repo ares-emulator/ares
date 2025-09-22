9e4a00b9680d7c758059c65a04aceb0a6eefcd69
Luke Usher <luke@usher.cc>
2025-09-22 13:37:55 +0100

    pc-engine-ld: detect and insert a bios hucard when a cd-rom is loaded that depends on it.
    Currently non-working because mia rejects non-ld content for LaserActive right now, but it's here for when we fix that!

--- desktop-ui/emulator/pc-engine-ld.cpp
+++ desktop-ui/emulator/pc-engine-ld.cpp
@@ -6,6 +6,7 @@ struct PCEngineLD : PCEngine {
 
   shared_pointer<mia::Pak> hucard;
   u32 internalBiosId = 0;
+  maybe<u32> hucardBiosId;
 };
 
 PCEngineLD::PCEngineLD() {
@@ -16,6 +17,8 @@ PCEngineLD::PCEngineLD() {
   firmware.push_back({"PAC-N1",  "Japan", "459325690a458baebd77495c91e37c4dddfdd542ba13a821ce954e5bb245627f"});
   firmware.push_back({"PCE-LP1", "Japan", "3f43b3b577117d84002e99cb0baeb97b0d65b1d70b4adadc68817185c6a687f0"});
 
+  firmware.push_back({"System-Card 1.0", "Japan", "afe9f27f91ac918348555b86298b4f984643eafa2773196f2c5441ea84f0c3bb"});
+  firmware.push_back({"Games Express"  , "Japan", "4b86bb96a48a4ca8375fc0109631d0b1d64f255a03b01de70594d40788ba6c3d"});
   allocatePorts();
 }
 
@@ -45,18 +48,40 @@ auto PCEngineLD::load() -> LoadResult {
     return result;
   }
 
+  // Some games require a specific card to be inserted into the system
+  if(auto requiredCard = game->pak->attribute("card")) {
+    if(requiredCard == "System Card 1.0") {
+      hucardBiosId = 0;
+    } else if(requiredCard == "Games Express") {
+      hucardBiosId = 3;
+    }
+  }
+
+  // If a hucard was required, mount it
   hucard = mia::Medium::create("PC Engine");
+  if (hucardBiosId) {
+    result = hucard->load(firmware[hucardBiosId.get()].location);
+    if(result != successful) {
+      result.firmwareSystemName = "PC Engine";
+      result.firmwareType = firmware[hucardBiosId.get()].type;
+      result.firmwareRegion = firmware[hucardBiosId.get()].region;
+      result.result = noFirmware;
+      return result;
+    }
+  }
+
 
   ares::PCEngine::option("Pixel Accuracy", settings.video.pixelAccuracy);
 
   auto name = "[Pioneer] LaserActive (NEC PAC)";
   if(!ares::PCEngine::load(root, {name, " (", region, ")"})) return otherError;
 
-  //TODO: If a hucard was selected/if the game requires a specific system card, insert it here
-  //if(auto port = root->find<ares::Node::Port>("Cartridge Slot")) {
-//    port->allocate();
-//    port->connect();
-//  }
+  if(hucardBiosId) {
+    if (auto port = root->find<ares::Node::Port>("Cartridge Slot")) {
+      port->allocate();
+      port->connect();
+    }
+  }
 
   if(auto port = root->find<ares::Node::Port>("PC Engine LD/Disc Tray")) {
     port->allocate();
