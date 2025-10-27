c763218430a0894411c9f97b7dd14d84df13a5a9
jcm <6864788+jcm93@users.noreply.github.com>
2025-10-27 16:32:20 -0500

    build: Add ARES_DEBUG_DEPENDENCIES build option

--- cmake/windows/dependencies.cmake
+++ cmake/windows/dependencies.cmake
@@ -7,8 +7,12 @@ function(_check_dependencies_windows)
   set(dependencies_dir "${CMAKE_CURRENT_SOURCE_DIR}/.deps")
   set(prebuilt_filename "ares-deps-windows-ARCH.tar.xz")
   set(prebuilt_destination "ares-deps-windows-ARCH")
-
   set(dependencies_list prebuilt)
+  if(ARES_DEBUG_DEPENDENCIES)
+    set(source_filename "ares-deps-windows-ARCH-source.tar.xz")
+    set(source_destination "ares-deps-windows-ARCH-source")
+    list(APPEND dependencies_list source)
+  endif()
 
   _check_dependencies()
 endfunction()
