ca2c70be91605d293dac02abdb1cbe99581f11ca
remutro <remulationtro@gmail.com>
2025-12-18 07:30:14 -0500

    Add noise channel to save state serialization

--- ares/fc/system/serialization.cpp
+++ ares/fc/system/serialization.cpp
@@ -1,4 +1,4 @@
-static const string SerializerVersion = "v150";
+static const string SerializerVersion = "v151";
 
 auto System::serialize(bool synchronize) -> serializer {
   if(synchronize) scheduler.enter(Scheduler::Mode::Synchronize);
