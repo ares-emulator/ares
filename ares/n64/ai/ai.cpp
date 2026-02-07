ae41a4356732803a0ee72280a0a7edab431d1a79
meauxdal <68095521+meauxdal@users.noreply.github.com>
2026-02-07 09:40:48 -0500

    n64: fix audio pops when AI DMA ends
    On a real console, when the AI DMA ends, even if the volume is not zero,
    there is a decay happening on the analog path that doesn't cause any
    audible pops. This can happen in games when they fail to meet the
    audio deadline and schedule a new buffer before the previous one ends.

--- ares/n64/ai/ai.cpp
+++ ares/n64/ai/ai.cpp
@@ -26,47 +26,57 @@ auto AI::unload() -> void {
 
 auto AI::main() -> void {
   while(Thread::clock < 0) {
-    f64 left = 0, right = 0;
-    sample(left, right);
-    stream->frame(left, right);
+    sample();
+    stream->frame(dac.left, dac.right);
     step(dac.period);
   }
 }
 
-auto AI::sample(f64& left, f64& right) -> void {
-  if(io.dmaCount == 0) return;
+auto AI::sample() -> void {
+  bool active = false;
 
-  if(io.dmaLength[0] && io.dmaEnable) {
+  if(io.dmaCount && io.dmaLength[0] && io.dmaEnable) {
     io.dmaAddress[0].bit(13,23) += io.dmaAddressCarry;
-    auto data  = rdram.ram.read<Word>(io.dmaAddress[0], "AI");
-    auto l     = s16(data >> 16);
-    auto r     = s16(data >>  0);
-    left       = l / 32768.0;
-    right      = r / 32768.0;
+    auto data = rdram.ram.read<Word>(io.dmaAddress[0], "AI");
+    dac.left  = (s16)(data >> 16) / 32768.0;
+    dac.right = (s16)(data >>  0) / 32768.0;
 
     io.dmaAddress[0].bit(0,12) += 4;
-    io.dmaAddressCarry          = io.dmaAddress[0].bit(0,12) == 0;
-    io.dmaLength[0]            -= 4;
+    io.dmaAddressCarry = io.dmaAddress[0].bit(0,12) == 0;
+    io.dmaLength[0] -= 4;
+    active = true;
   }
-  if(!io.dmaLength[0]) {
+
+  if(io.dmaCount && io.dmaLength[0] == 0) {
     if(--io.dmaCount) {
       io.dmaAddress[0]  = io.dmaAddress[1];
-      io.dmaLength [0]  = io.dmaLength [1];
+      io.dmaLength[0]   = io.dmaLength[1];
       io.dmaOriginPc[0] = io.dmaOriginPc[1];
       mi.raise(MI::IRQ::AI);
     }
   }
+
+  if(!active) {
+    dac.left  *= dac.decayFactor;
+    dac.right *= dac.decayFactor;
+    if(fabs(dac.left)  < 1e-7) dac.left  = 0.0;
+    if(fabs(dac.right) < 1e-7) dac.right = 0.0;
+  }
+}
+
+auto AI::updateDecay() -> void {
+  dac.decayFactor = exp(-1.0 / (dac.frequency * 0.003));
 }
 
 auto AI::power(bool reset) -> void {
   Thread::reset();
-
-  fifo[0] = {};
-  fifo[1] = {};
   io = {};
+  dac.left  = 0.0;
+  dac.right = 0.0;
   dac.frequency = 44100;
   dac.precision = 16;
-  dac.period    = system.frequency() / dac.frequency;
+  dac.period = system.frequency() / dac.frequency;
+  updateDecay();
 }
 
 }
