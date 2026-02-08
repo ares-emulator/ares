7ad5ba5363b966d91276e0d977922027c6033f0f
meauxdal <68095521+meauxdal@users.noreply.github.com>
2026-02-08 01:25:33 -0500

    starve v42
    mi.raise(MI::IRQ::AI) ordering

--- ares/n64/ai/ai.cpp
+++ ares/n64/ai/ai.cpp
@@ -1,5 +1,4 @@
 #include <n64/n64.hpp>
-#include <cmath>
 
 namespace ares::Nintendo64 {
 
@@ -34,50 +33,49 @@ auto AI::main() -> void {
 }
 
 auto AI::sample() -> void {
+  bool active = false;
+
   if(io.dmaCount && io.dmaLength[0] && io.dmaEnable) {
     io.dmaAddress[0].bit(13,23) += io.dmaAddressCarry;
     auto data = rdram.ram.read<Word>(io.dmaAddress[0], "AI");
-    auto l = s16(data >> 16);
-    auto r = s16(data >> 0);
-    dac.left = l / 32768.0;
-    dac.right = r / 32768.0;
+    dac.left  = (s16)(data >> 16) / 32768.0;
+    dac.right = (s16)(data >>  0) / 32768.0;
 
     io.dmaAddress[0].bit(0,12) += 4;
     io.dmaAddressCarry = io.dmaAddress[0].bit(0,12) == 0;
     io.dmaLength[0] -= 4;
-  } else {
-    dac.left *= dac.decayFactor;
-    dac.right *= dac.decayFactor;
-    if(fabs(dac.left) < 1e-7) dac.left = 0.0;
-    if(fabs(dac.right) < 1e-7) dac.right = 0.0;
+    active = true;
   }
 
-  if(io.dmaCount && !io.dmaLength[0]) {
-    mi.raise(MI::IRQ::AI);
+  if(io.dmaCount && io.dmaLength[0] == 0) {
     if(--io.dmaCount) {
-      io.dmaAddress[0] = io.dmaAddress[1];
-      io.dmaLength[0] = io.dmaLength[1];
+      io.dmaAddress[0]  = io.dmaAddress[1];
+      io.dmaLength[0]   = io.dmaLength[1];
       io.dmaOriginPc[0] = io.dmaOriginPc[1];
+      mi.raise(MI::IRQ::AI);
     }
+  }
+
+  if(!active) {
+    dac.left  *= dac.decayFactor;
+    dac.right *= dac.decayFactor;
+    if(fabs(dac.left)  < 1e-7) dac.left  = 0.0;
+    if(fabs(dac.right) < 1e-7) dac.right = 0.0;
   }
 }
 
 auto AI::updateDecay() -> void {
-  f64 time = 0.003;
-  dac.decayFactor = exp(-1.0 / (dac.frequency * time));
+  dac.decayFactor = exp(-1.0 / (dac.frequency * 0.003));
 }
 
 auto AI::power(bool reset) -> void {
   Thread::reset();
-
-  fifo[0] = {};
-  fifo[1] = {};
   io = {};
+  dac.left  = 0.0;
+  dac.right = 0.0;
   dac.frequency = 44100;
   dac.precision = 16;
   dac.period = system.frequency() / dac.frequency;
-  dac.left = 0.0;
-  dac.right = 0.0;
   updateDecay();
 }
 
