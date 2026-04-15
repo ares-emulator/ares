b312aafb6762d78ca4d1cd91640203e1c18e2ed0
SternXD <stern@sidestore.io>
2026-04-14 23:03:41 -0400

    ruby: Add Direct3D 12 display backend

--- ruby/video/video.cpp
+++ ruby/video/video.cpp
@@ -2,6 +2,10 @@
   #include <ruby/video/direct3d9.cpp>
 #endif
 
+#if defined(VIDEO_DIRECT3D12)
+  #include <ruby/video/direct3d12/d3d12device.cpp>
+#endif
+
 #if defined(VIDEO_GLX)
   #include <ruby/video/glx.cpp>
 #endif
@@ -175,6 +179,10 @@ auto Video::create(string driver) -> bool {
   if(driver == "Direct3D 9.0") self.instance = std::make_unique<VideoDirect3D9>(*this);
   #endif
 
+  #if defined(VIDEO_DIRECT3D12)
+  if(driver == "Direct3D 12") self.instance = std::make_unique<d3d12device>(*this);
+  #endif
+
   #if defined(VIDEO_GLX)
   if(driver == "OpenGL 3.2") self.instance = std::make_unique<VideoGLX>(*this);
   #endif
@@ -204,6 +212,10 @@ auto Video::hasDrivers() -> std::vector<string> {
   "Direct3D 9.0",
   #endif
 
+  #if defined(VIDEO_DIRECT3D12)
+  "Direct3D 12",
+  #endif
+
   #if defined(VIDEO_GLX)
   "OpenGL 3.2",
   #endif
@@ -224,6 +236,8 @@ auto Video::optimalDriver() -> string {
   return "OpenGL 3.2";
   #elif defined(VIDEO_DIRECT3D9)
   return "Direct3D 9.0";
+  #elif defined(VIDEO_DIRECT3D12)
+  return "Direct3D 12";
   #else
   return "None";
   #endif
@@ -234,6 +248,8 @@ auto Video::safestDriver() -> string {
   return "Metal";
   #elif defined(VIDEO_DIRECT3D9)
   return "Direct3D 9.0";
+  #elif defined(VIDEO_DIRECT3D12)
+  return "Direct3D 12";
   #elif defined(VIDEO_WGL)
   return "OpenGL 3.2";
   #elif defined(VIDEO_GLX)
