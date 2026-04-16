49eaa3af73ddab6b9f88459248068c6b8c3446fa
NovaSquirrel <bushytail@novasquirrel.com>
2026-04-16 19:56:29 -0400

    snes: Fix Super Game Boy $6002 and $700x registers
    The Super Game Boy only stores one packet, and packet acknowledgement is done by reading $7000, not $6002

--- ares/sfc/coprocessor/icd/icd.cpp
+++ ares/sfc/coprocessor/icd/icd.cpp
@@ -54,16 +54,13 @@ auto ICD::power(bool reset) -> void {
   });
   cpu.coprocessors.push_back(this);
 
-  for(auto& packet : this->packet) packet = {};
-  packetSize = 0;
-
   joypID = 3;
   joypLock = 1;
   pulseLock = 1;
   strobeLock = 0;
   packetLock = 0;
-  joypPacket = {};
   packetOffset = 0;
+  packetReady = 0;
   bitData = 0;
   bitOffset = 0;
 
--- ares/sfc/coprocessor/icd/serialization.cpp
+++ ares/sfc/coprocessor/icd/serialization.cpp
@@ -2,16 +2,13 @@ auto ICD::serialize(serializer& s) -> void {
   Thread::serialize(s);
   GameBoy::system.serialize(s, scheduler.getSynchronize());
 
-  for(u32 n : range(64)) s(packet[n].data);
-  s(packetSize);
-
   s(joypID);
   s(joypLock);
   s(pulseLock);
   s(strobeLock);
   s(packetLock);
-  s(joypPacket.data);
   s(packetOffset);
+  s(packetReady);
   s(bitData);
   s(bitOffset);
 
